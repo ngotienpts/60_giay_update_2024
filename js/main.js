54aabb8e29217fbb26c7e69292639ad5167b1e9c
Tien <ngotienpts@gmail.com>
2024-09-18 16:36:56 +0700

    18/9

---
+++ js/main.js
@@ -0,0 +1,98 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Tập hợp tất cả các phần tử cần sử dụng
+    const backTop = document.querySelector("#back-top");
+    const stickyHeaderPC = document.querySelector(".js__stickyHeader");
+    const video169s = document.querySelectorAll(".js__video169");
+
+    // search mb
+    const searchMbs = document.querySelectorAll(".js__searchMb");
+
+
+    // Xử lý sự kiện khi nhấn nút "back to top"
+    function handleBackTop() {
+        if (!backTop) return;
+
+        backTop.onclick = function () {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        };
+
+    }
+
+
+
+    // Xử lý video tỉ lệ 16:9
+    function handleVideo169() {
+        if (video169s) {
+            video169s.forEach((video169) => {
+                var videos = video169.querySelectorAll("iframe");
+                if (videos) {
+                    videos.forEach((video) => {
+                        var w = video.offsetWidth;
+                        video.style.height = (w * 9) / 16 + "px";
+                    });
+                }
+            });
+        }
+    }
+
+    // Xử lý sự kiện show search mb
+    function handleShowSearchMb() {
+        if (!searchMbs) return;
+        searchMbs.forEach((searchMb) => {
+            var closeSearchMb =
+                document.querySelector(".js__closeSearchMb");
+            var formSearchMb = document.querySelector(".js__formSearchMb");
+            const focusElement =
+                formSearchMb.querySelector(".js__focusSearchMb");
+            searchMb.onclick = function () {
+                formSearchMb.classList.add("active");
+                focusElement.focus();
+            };
+            closeSearchMb.onclick = function () {
+                formSearchMb.classList.remove("active");
+                focusElement.value = "";
+            };
+        });
+    }
+    // Xử lý thanh header dính
+    function handleStickyHeader() {
+        if (stickyHeaderPC) {
+            const isSticky = scrollY > 300;
+            stickyHeaderPC.classList.toggle("sticky", isSticky);
+        }
+    }
+    // Hàm hiển thị nút backTop dựa trên vị trí cuộn trang
+    function handleBackTopVisibility() {
+        if (backTop) {
+            if (
+                document.body.scrollTop > 300 ||
+                document.documentElement.scrollTop > 300
+            ) {
+                backTop.style.opacity = 1;
+                backTop.style.visibility = "visible";
+            } else {
+                backTop.style.opacity = 0;
+                backTop.style.visibility = "hidden";
+            }
+        }
+    }
+    // Xử lý sự kiện khi cuộn trang
+    function handleWindowScroll() {
+        window.onscroll = function () {
+            handleStickyHeader();
+            handleBackTopVisibility()
+        };
+    }
+
+    // Khởi tạo tất cả các chức năng
+    function initApp() {
+        handleBackTop();
+        handleShowSearchMb();
+        handleVideo169();
+        handleWindowScroll();
+    }
+
+    // Bắt đầu khởi tạo ứng dụng
+    initApp();
+});
